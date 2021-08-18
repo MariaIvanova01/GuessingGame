421a33372b44f0c5a677645363e99889c15941d9
MariaIvanova01 <mimi200801@abv.bg>
2021-08-18 16:41:46 +0300

    Initial commit

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string secretWord = "rain";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 10;
+            bool noGuesses = false;
+
+            while (guess != secretWord && !noGuesses)
+            {
+                if ( guessCount < guessLimit)
+                {
+                    Console.Write("Enter word : ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    noGuesses = true;
+                }
+                
+            }
+            if (noGuesses)
+            {
+                Console.WriteLine(" Sorry. YOU LOSE!");
+            }
+            else
+            {
+                Console.WriteLine(" CONGRATULATIONS! ");
+
+            }
+           
+
+        }
+    }
+}
